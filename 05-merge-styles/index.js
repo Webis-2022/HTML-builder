ca4230b38677c53d0f9dc84dcb92086f92cc09ab
Webis-2022 <verjovkin.jevgeni@gmail.com>
2025-01-21 15:49:58 +0200

    fix: syntactic issue in 5-th task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,24 +3,27 @@ const path = require("path");
 
 async function copyStyles(sourceDir, outputFile) {
   try {
-
-    const files = await fs.readdir("styles", { withFileTypes: true });
+    const files = await fs.readdir(sourceDir, { withFileTypes: true });
     const styles = [];
-    for(let file of files) {
+
+    for (const file of files) {
       const sourcePath = path.join(sourceDir, file.name);
       const extension = path.extname(file.name);
-      if(file.isFile() && extension === ".css") {
-        const data =  await fs.readFile(sourcePath, "utf-8", { withFileTypes: true });
+
+      if (file.isFile() && extension === ".css") {
+        const data = await fs.readFile(sourcePath, "utf-8");
         styles.push(data);
       }
     }
-    await fs.writeFile(outputFile, styles.join("\n")), "utf-8"
+
+    await fs.writeFile(outputFile, styles.join("\n"), "utf-8");
     console.log("Success!");
   } catch (err) {
-    console.log(err.message);
+    console.error("Error:", err.message);
   }
 }
 
-const source = "./styles";
-const output = "./project-dist/bundle.css"
+const source = path.join(__dirname, "styles");
+const output = path.join(__dirname, "project-dist", "bundle.css");
+
 copyStyles(source, output);
