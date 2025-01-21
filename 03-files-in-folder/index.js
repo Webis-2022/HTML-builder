3d7084473386d184d77551eed1c6b3eeac379e89
Webis-2022 <verjovkin.jevgeni@gmail.com>
2025-01-21 15:35:03 +0200

    fix: change computing of Kb

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,21 +3,22 @@ const path = require("path");
 
 async function listFilesInfo(folderPath) {
   try {
-    const files = await fs.readdir(folderPath, { withFileTypes: true});
+    const files = await fs.readdir(folderPath, { withFileTypes: true });
     for (const file of files) {
       if (file.isFile()) {
         const filePath = path.join(folderPath, file.name);
         const stats = await fs.stat(filePath);
         const extension = path.extname(file.name);
-        const indexOfName = file.name.indexOf('.');
-        const cleanedName = file.name.substring(file.name[file.name.length -1], indexOfName);
-        const cleanedExtension = extension.replace('.', "")
+        const indexOfName = file.name.lastIndexOf(".");
+        const cleanedName = file.name.substring(0, indexOfName);
+        const cleanedExtension = extension.replace(".", "");
+        const fileSizeInKB = (stats.size / 1024).toFixed(2);
 
-        console.log(`${cleanedName} - ${cleanedExtension} - ${stats.size / 1000} Kb`)
+        console.log(`${cleanedName} - ${cleanedExtension} - ${fileSizeInKB} KB`);
       }
     }
   } catch (err) {
-    console.log ("Err", err)
+    console.log("Error:", err);
   }
 }
 
